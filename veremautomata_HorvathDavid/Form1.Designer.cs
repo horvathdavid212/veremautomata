f56adb6a9b86b026c3ab3a5410531e40c07f66b7
Horváth Dávid <77624848+horvathdavid212@users.noreply.github.com>
2023-12-12 18:39:14 +0100

    Add files via upload

---
+++ veremautomata_HorvathDavid/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace veremautomata_HorvathDavid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(362, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(402, 288);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Location = new System.Drawing.Point(237, 48);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(106, 46);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(95, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 31);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(30, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(274, 288);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox2.Location = new System.Drawing.Point(516, 48);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(248, 36);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(357, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Példa szabály:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Veremautómata beadandó";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ veremautomata_HorvathDavid/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veremautomata_HorvathDavid
+{
+    public partial class Form1 : Form
+    {
+        string inputSzalag;
+        int index = 0; // Indexelés
+        Stack<string> stack = new Stack<string>(); // Verem
+        List<string> alkalmazottSzabalyok = new List<string>(); // Alkalmazott szabályok listája
+        string[,] rulesMatrix; // szabály táblázat a mátrixban
+        bool hiba = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeRulesMatrix();
+            InitializeDataGridView();
+            stack.Push("#");
+            stack.Push("E");
+            richTextBox2.Text = "i+i; i+i*(i+i); (i+i)*i";
+
+        }
+
+        private void InitializeRulesMatrix()
+        {
+            // Mátrix inicializálása az adott táblázat adataival
+            rulesMatrix = new string[,] {
+            //  +       *       (       )       i       #
+            { null,   null,   "TE',1", null,   "TE',1", null   },   // E
+            { "+TE',2", null,   null,   "e,3",   null,   "e,3"   }, // E'
+            { null,   null,   "FT',4", null,   "FT',4", null   },   // T
+            { "e,6",  "*FT',5", null,   "e,6",   null,   "e,6"   }, // T'
+            { null,   null,   "(E),7", null,   "i,8",   null   },   // F
+            { "pop",  null,   null,   null,   null,   null   },     // +
+            { null,   "pop",  null,   null,   null,   null   },     // *
+            { null,   null,   "pop",  null,   null,   null   },     // (
+            { null,   null,   null,   "pop",  null,   null   },     // )
+            { null,   null,   null,   null,   "pop",  null   },     // i
+            { null,   null,   null,   null,   null,   "elfogad" }   // #
+        };
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Oszlopok hozzáadása
+            dataGridView1.Columns.Add("+", "+");
+            dataGridView1.Columns.Add("*", "*");
+            dataGridView1.Columns.Add("(", "(");
+            dataGridView1.Columns.Add(")", ")");
+            dataGridView1.Columns.Add("i", "i");
+            dataGridView1.Columns.Add("#", "#");
+
+            // Az oszlopok fejléceinek beállítása
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.HeaderCell.Value = col.Name;
+            }
+
+            // A sorfejlécek megadása
+            string[] rowHeaders = { "E", "E'", "T", "T'", "F", "+", "*", "(", ")", "i", "#" };
+
+            // A mátrix sorainak hozzáadása a DataGridView-hoz
+            for (int row = 0; row < rulesMatrix.GetLength(0); row++)
+            {
+                // új sor létrehozása a mátrix adataival
+                DataGridViewRow newRow = new DataGridViewRow();
+                newRow.CreateCells(dataGridView1);
+
+                // sorfejléc beállítása
+                newRow.HeaderCell.Value = rowHeaders[row];
+
+                for (int col = 0; col < rulesMatrix.GetLength(1); col++)
+                {
+                    // cellák kitöltése a mátrix adataival
+                    newRow.Cells[col].Value = rulesMatrix[row, col];
+                }
+
+                // sor hozzáadása a DataGridView-hoz
+                dataGridView1.Rows.Add(newRow);
+            }
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.RowHeadersWidth = 50;
+            dataGridView1.RowHeadersVisible = true;
+        }
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // alaphelyzetbe állítás
+            ResetApplicationState();
+            // folyamatosan dolgozza fel az input szalagot, amíg nem ér véget, vagy a verem ki nem ürül
+            while (index < inputSzalag.Length && stack.Count > 0 && !hiba)
+            {
+                
+                ProcessInput(); // felddolgozza a jelenlegi input karaktert és a verem tetején lévő elemet
+
+                // megáll, ha a verem kiürült vagy csak a # marad benne
+                if (stack.Count == 0 || (stack.Count > 0 && stack.Peek() == "#"))
+                {
+                    break;
+                }
+            }
+
+            // ellenőrzi, hogy a veremben csak a '#' maradt-e, ami a helyes feldolgozás jele
+            if (stack.Count == 1 && stack.Peek() == "#")
+            {
+                //MessageBox.Show("A kifejezés helyes.");
+            }
+            else
+            {
+                //MessageBox.Show("index == inputTape.Length ??: " + index +"=?"+ inputTape.Length);
+                MessageBox.Show("A kifejezés helytelen vagy nem teljes.");
+            }
+        }
+
+        private void ProcessInput()
+        {
+            // a jelenlegi input karakter és a verem tetején lévő elem lekérdezése
+            string currentInput = inputSzalag[index].ToString();
+            string topStack = stack.Peek();
+
+            ApplyRule(currentInput, topStack);
+        }
+
+        private void ApplyRule(string input, string stackTop)
+        {
+            // itt meghatározza a jelenlegi állapotnak megfelelő sor és oszlopindexeket a szabálymátrixban
+            int rowIndex = GetRowIndex(stackTop);
+            int colIndex = GetColumnIndex(input);
+
+            //MessageBox.Show("Row: " + rowIndex + ", Col: " + colIndex);
+
+            // van-e érvényes szabály vagy null-ra mutat a mátrixban
+            if (rulesMatrix[rowIndex, colIndex] == null && !hiba)
+            {
+                MessageBox.Show("Hiba: nincs szabály. Row: " + rowIndex+1 + ", Col: " + colIndex+1);
+                ResetApplicationState();
+                hiba = true;
+                return;
+            }
+            if (!hiba)
+            { // szabály feldolgozása
+                string rule = rulesMatrix[rowIndex, colIndex];
+                if (rule.StartsWith("pop"))
+                {
+                    // ha a sor és oszlop megegyezik akkor törlés és index++
+                    stack.Pop();
+                    index++;
+                }
+                else
+                {
+                    // szabály elemeinek hozzáadása a veremhez
+                    string[] ruleParts = rule.Split(',');
+                    string ruleToApply = ruleParts[0];
+                    string ruleNumber = ruleParts[1];
+                    alkalmazottSzabalyok.Add(ruleNumber);
+
+                    stack.Pop(); // eltávolítjuk a verem tetejéről az elemet
+
+                    // add új elemeket a veremhez, figyelembe véve a speciális "E'" és "T'" eseteket
+                    if (ruleToApply != "e")
+                    {
+                        for (int i = ruleToApply.Length - 1; i >= 0; i--)
+                        {
+                            if (i > 0 && (ruleToApply[i] == '\'' && (ruleToApply[i - 1] == 'E' || ruleToApply[i - 1] == 'T')))
+                            {
+                                stack.Push(ruleToApply.Substring(i - 1, 2)); // hozzáadom az 'E'' vagy 'T''-t
+                                i--; // kihagyjuk a következő karaktert, mivel az már része a fenti stringnek
+                            }
+                            else
+                            {
+                                stack.Push(ruleToApply[i].ToString());
+                            }
+                        }
+                    }
+                }
+                UpdateUI();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private int GetRowIndex(string stackTop)
+        {
+            // sorindexeket határozza meg a mátrixban
+            string rowHeaders = "E,E',T,T',F,+,*,(,),i,#";
+            string[] rows = rowHeaders.Split(',');
+            for (int i = 0; i < rows.Length; i++)
+            {
+                //MessageBox.Show("rows i: "+rows[i]+"\n"+ "stackTop: " + stackTop);
+                if (stackTop == rows[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private int GetColumnIndex(string input)
+        {
+            // oszlopindexeket határozza meg a mátrixban
+            string colHeaders = "+,*,(,),i,#";
+            string[] cols = colHeaders.Split(',');
+            for (int i = 0; i < cols.Length; i++)
+            {
+                if (input == cols[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void ResetApplicationState()
+        {
+            // alaphelyzetbe állítás
+            richTextBox1.Text = "";
+            alkalmazottSzabalyok.Clear();
+            stack.Clear();
+            stack.Push("#");
+            stack.Push("E");
+            index = 0;
+            hiba = false;
+            inputSzalag = textBox1.Text + "#";
+        }
+
+        private void UpdateUI()
+        {
+            // kiíratás
+            richTextBox1.AppendText(inputSzalag.Substring(index) + ", ");
+            richTextBox1.AppendText(string.Join("", stack) + ", ");
+            richTextBox1.AppendText(String.Join("", alkalmazottSzabalyok) + "\n");
+            if (stack.Count == 1 && stack.Peek() == "#")
+            {
+                richTextBox1.AppendText("Elfogadva\n");
+            }
+        }
+    }
+}
